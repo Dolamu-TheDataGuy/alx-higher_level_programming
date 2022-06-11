8c3042a2c9943d422ff638f9028409dbd716fd87
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-06-10 20:09:59 -0700

    added replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    n = len(my_list)
+    for i in range(n-1):
+        if my_list[i] == search:
+            my_list[i] = replace
+    return my_list
