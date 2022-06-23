bdcfff99c494a9599a353b6b63b8082a0ff009cb
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-06-22 20:27:54 -0700

    added changes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    cmpt = 0
-    for index in range(x):
+    element_count = 0
+    for i in range(x):
         try:
-            print(my_list[index], end='')
-            cmpt += 1
-        except Exception as error:
+            print("{}".format(my_list[i]), end='')
+        except IndexError:
             break
-    print('')
-    return 
+        else:
+            element_count += 1
+    print()
+    return (element_count)
