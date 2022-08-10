0166acea244990a1fed44361ca770977acb1341d
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-08-10 16:29:47 -0700

    added 2changes

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
---creates a table called firsttable in my current DB
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+-- Creates a table called firsttable in my current DB
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
