6af63006194e54074e437e9d6459eec9a8822330
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-11-13 21:55:35 +0100

    request

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     #made the request get with the url
     #r = requests.get(sys.argv[1])
     r = requests.get(sys.argv[1])
-    # used the headers.ger to get requests
-    #req = r.headers.get('X-Request-Id')
+    # used the headers.get to get requests
+    #req = r.headers.get("X-Request-Id")
     #print(req)
     print(r.headers.get("X-Request-Id"))
