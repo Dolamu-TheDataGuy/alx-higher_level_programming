75ef2c7a789ca3bb62a940cce9b0f33adc4e3892
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-11-13 21:50:51 +0100

    request

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
-Use requests package to make a get request to given URL and display
-the value of `X-Request-Id` in response header.
+Python script that sends a request to the URL and
+displays the value of a variable in the response header
 """
-import sys
 import requests
+import sys
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    r = requests.get(url)
-    print(r.headers.get('x-request-id'))
+    #made the request get with the url
+    #r = requests.get(sys.argv[1])
+    r = requests.get(sys.argv[1])
+    # used the headers.ger to get requests
+    #req = r.headers.get('X-Request-Id')
+    #print(req)
+    print(r.headers.get('X-Request-Id'))
