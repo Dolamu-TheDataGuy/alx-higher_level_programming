52cd5df02d747b5fc787405d158e15de34faa0ec
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-11-13 21:58:28 +0100

    request

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that sends a request to the URL and
-displays the value of a variable in the response header
+""" A Python script that sends a request to a URL and extract
+    information in the header of the response
 """
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    #made the request get with the url
-    #r = requests.get(sys.argv[1])
-    r = requests.get(sys.argv[1])
-    # used the headers.get to get requests
-    #req = r.headers.get("X-Request-Id")
-    #print(req)
-    print(r.headers.get("X-Request-Id"))
+    res = requests.get(sys.argv[1])
+    print(res.headers.get("X-Request-Id"))
