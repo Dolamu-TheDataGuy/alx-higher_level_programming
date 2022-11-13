eac36ce22f872a3d72b93d186d48c0d329d722f0
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-11-13 22:09:36 +0100

    request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""
-Use requests package to make a post request to given URL with argument
-set in variable `q`, defaulting to empty string. If response body is properly
-JSON formatted and not empty, display `id` and `name` as given format.
-Otherwise display error message.
+""" A Python script that takes in a letter sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter q as a parameter
 """
 import sys
 import requests
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        arg = sys.argv[1]
-    else:
-        arg = ""
-    payload = {'q': arg}
-    url = "http://0.0.0.0:5000/search_user"
-    r = requests.post(url, data=payload)
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    res = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        r.raise_for_status()
-        json = r.json()
-        if len(json) == 0:
+        res_json = res.json()
+        if res_json == {}:
             print("No result")
         else:
-            print("[{:d}] {}".format(json['id'], json['name']))
-    except Exception:
+            print("[{}] {}".format(res_json.get("id"), res_json.get("name")))
+    except ValueError:
         print("Not a valid JSON")
