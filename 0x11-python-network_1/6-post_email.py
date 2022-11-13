0f349f3bc8aec2ef0b338e941df0dededd803f10
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-11-13 22:01:37 +0100

    request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+""" A script that takes in a URL and sends a POST request to
+    the URL with an email as a parameter and displays the body
+    of the response
 """
-Use requests package to make a post request sending email param
-and display body of response.
-"""
-import sys
 import requests
+import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    payload = {'email': sys.argv[2]}
-    r = requests.post(url, data=payload)
+    value = {"email": sys.argv[2]}
+    r = requests.post(url, data=value)
     print(r.text)
